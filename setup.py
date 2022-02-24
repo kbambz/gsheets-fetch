8b5ee8dc71724c0d076e94420f546879f35313eb
Kate Bambino <katie.bambino@gmail.com>
2022-02-24 11:14:17 -0500

    Bump setup.py version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name='gsheets_fetch',
     description='Download sheets from Google Sheets as CSV, plain text, or JSON files.',
-    version='0.0.1',
+    version='0.0.3',
     url='https://github.com/kbambz/gsheets-fetch',
     author='Kathryn Bambino',
     author_email='katie.bambino@gmail.com',
@@ -13,9 +13,9 @@ setup(
     py_modules=['gsheets_fetch'],
     include_package_data=True,
     install_requires=[
-        'google-api-python-client==1.7.8',
-        'google-auth-httplib2==0.0.3',
-        'google-auth-oauthlib==0.2.0',
+        'google-api-python-client==2.37.0',
+        'google-auth-httplib2==0.1.0',
+        'google-auth-oauthlib==0.4.6',
     ],
     entry_points='''
         [console_scripts]
